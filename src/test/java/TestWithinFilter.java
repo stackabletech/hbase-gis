655923a19d69d1f97deef5b4aa48220e39b09dbb
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-09-27 15:29:01 +0200

    add easily-verifiable tests

--- src/test/java/TestWithinFilter.java
+++ src/test/java/TestWithinFilter.java
@@ -14,13 +14,16 @@ import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.InternalScanner;
 import org.apache.hadoop.hbase.regionserver.Region;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
@@ -28,6 +31,7 @@ import static org.junit.Assert.assertEquals;
 public class TestWithinFilter {
     private final static Logger LOG = LoggerFactory.getLogger(TestWithinFilter.class);
     private final static HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
+    public static final int POINT_COUNT = 10;
 
     private static HRegion REGION;
     private static final byte[] FAMILY = "a".getBytes();
@@ -39,20 +43,31 @@ public class TestWithinFilter {
             .trimResults()
             .limit(COLUMNS.length);
 
-    private static final byte[] ID = "id".getBytes();
     private static final byte[] X_COL = "lon".getBytes();
     private static final byte[] Y_COL = "lat".getBytes();
+
+    private static final byte[][] COLUMNS_SCAN = {"id".getBytes(), X_COL, Y_COL};
+
+    //private static final byte[] FAMILY_RECTANGLE_CHECK = "r".getBytes();
+    private static final byte[][] COLUMNS_RECTANGLE_CHECK = {X_COL, Y_COL};
 
     @BeforeClass
     public static void before() throws Exception {
         HTableDescriptor htd = new HTableDescriptor(TableName.valueOf("TestWithinFilter"));
         HColumnDescriptor family = new HColumnDescriptor(FAMILY).setVersions(100, 100);
+        //HColumnDescriptor family_rectangle_check = new HColumnDescriptor(FAMILY).setVersions(100, 100);
         htd.addFamily(family);
+        //htd.addFamily(family_rectangle_check);
         HRegionInfo info = new HRegionInfo(htd.getTableName(), null, null, false);
 
         REGION = HBaseTestingUtility.createRegionAndWAL(info, TEST_UTIL.getDataTestDir(),
                 TEST_UTIL.getConfiguration(), htd);
 
+        load_wifi_data();
+        load_rectangle_check_data();
+    }
+
+    private static void load_wifi_data() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/wifi_4326.txt"));
         reader.readLine(); // ignore header
         String line;
@@ -93,28 +108,33 @@ public class TestWithinFilter {
         LOG.debug(String.format("Geohashed %s records in %sms.", records, end - start));
     }
 
+    private static void load_rectangle_check_data() throws Exception {
+        // add a dataset consisting of a straight line y=x
+        for (Integer i = 1; i <= POINT_COUNT; i++) {
+            Put p = new Put(BigInteger.valueOf(i).toByteArray());
+            p.setDurability(Durability.SKIP_WAL);
+            for (byte[] column : COLUMNS_RECTANGLE_CHECK) {
+                p.addColumn(FAMILY, column, (i +".0").getBytes());
+            }
+            REGION.put(p);
+        }
+
+        REGION.flush(true);
+    }
+
     @AfterClass
     public static void after() throws Exception {
         HBaseTestingUtility.closeRegionAndWAL(REGION);
     }
 
     @Test
-    public void testScanAll() throws Exception {
-        WKTReader reader = new WKTReader();
-
-        String polygon = "POLYGON ((-73.980844 40.758703, " +
-                "-73.987214 40.761369, " +
-                "-73.990839 40.756400, " +
-                "-73.984422 40.753642, " +
-                "-73.980844 40.758703))";
-
-        Geometry query = reader.read(polygon);
-        int results = queryWithFilterAndRegionScanner(query, REGION, new FilterList());
-        assertEquals(1224, results);
+    public void testWithoutFilter() throws Exception {
+        int results = queryWithFilterAndRegionScanner(REGION, new FilterList(), FAMILY, COLUMNS_SCAN);
+        assertEquals(1234, results);
     }
 
     @Test
-    public void testScanWithFilter() throws Exception {
+    public void testWithPolygonFilter() throws Exception {
         WKTReader reader = new WKTReader();
 
         String polygon = "POLYGON ((-73.980844 40.758703, " +
@@ -126,12 +146,12 @@ public class TestWithinFilter {
         Geometry query = reader.read(polygon);
         Filter withinFilter = new WithinFilter(query);
         Filter filters = new FilterList(withinFilter);
-        int results = queryWithFilterAndRegionScanner(query, REGION, filters);
+        int results = queryWithFilterAndRegionScanner(REGION, filters, FAMILY, COLUMNS_SCAN);
         assertEquals(26, results);
     }
 
     @Test
-    public void testRectangleScanWithFilter() throws Exception {
+    public void testWithReducedPolygonFilter() throws Exception {
         WKTReader reader = new WKTReader();
 
         String polygon = "POLYGON ((-73.980844 40.758703, " +
@@ -142,19 +162,42 @@ public class TestWithinFilter {
         Geometry query = reader.read(polygon);
         Filter withinFilter = new WithinFilter(query);
         Filter filters = new FilterList(withinFilter);
-        int results = queryWithFilterAndRegionScanner(query, REGION, filters);
+        int results = queryWithFilterAndRegionScanner(REGION, filters, FAMILY, COLUMNS_SCAN);
         assertEquals(10, results);
     }
 
-    private int queryWithFilterAndRegionScanner(Geometry query, Region region, Filter filters) throws IOException {
+    @Test
+    public void testLinePointsWithoutFilter() throws Exception {
+        int results = queryWithFilterAndRegionScanner(REGION, new FilterList(), FAMILY, COLUMNS_RECTANGLE_CHECK);
+        assertEquals(1234, results);
+    }
+
+    @Test
+    public void testLinePointsWithRectangleFilter() throws Exception {
+        WKTReader reader = new WKTReader();
+
+        String polygon = "POLYGON ((0.0 0.0, " +
+                "0.0 3.0, " +
+                "3.0 3.0, " +
+                "3.0 0.0," +
+                "0.0 0.0))";
+
+        Geometry query = reader.read(polygon);
+        Filter withinFilter = new WithinFilter(query);
+        Filter filters = new FilterList(withinFilter);
+        int results = queryWithFilterAndRegionScanner(REGION, filters, FAMILY, COLUMNS_RECTANGLE_CHECK);
+        assertEquals(2, results);
+    }
+
+    private int queryWithFilterAndRegionScanner(Region region, Filter filters, byte[] family, byte[][] columnsScan) throws IOException {
         Scan scan = new Scan();
         scan.setFilter(filters);
-        scan.addFamily(FAMILY);
+        scan.addFamily(family);
         scan.readVersions(1);
         scan.setCaching(50);
-        scan.addColumn(FAMILY, ID);
-        scan.addColumn(FAMILY, X_COL);
-        scan.addColumn(FAMILY, Y_COL);
+        for (byte[] column : columnsScan) {
+            scan.addColumn(family, column);
+        }
 
         InternalScanner scanner = region.getScanner(scan);
         int i = 0;
@@ -168,7 +211,7 @@ public class TestWithinFilter {
 
             if (!results.isEmpty()) {
                 i++;
-                sb.append("Row:");
+                sb.append("Columns -");
 
                 for (Cell cell : results) {
                     sb.append(" ");
