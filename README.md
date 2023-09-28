b50a743a07b24f150053d8e2754b2e88c7876a3f
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-09-28 17:36:49 +0200

    added documentation and data limits as parameters

--- README.md
+++ README.md
@@ -44,6 +44,10 @@ Load data
 
     java -cp '/tmp/hbase-stackable/local/jars/hbase-gis-1.0.0.jar:/stackable/conf' tech.stackable.gis.hbase.Ingest wifi /tmp/wifi_4326.txt
 
+Load bulk data
+
+    java -cp '/tmp/hbase-stackable/local/jars/hbase-gis-1.0.0.jar:/stackable/conf' tech.stackable.gis.hbase.BulkIngest wifi a 100000 -75.99,-75.01,44.01,44.99
+
 ## Using the WithinFilter
 
     java -cp '/tmp/hbase-stackable/local/jars/hbase-gis-1.0.0.jar:/stackable/conf' \
@@ -56,6 +60,19 @@ Load data
         -73.980844 40.758703))"
 
 You should get 26 results.
+
+For the bulk-loaded data
+
+    java -cp '/tmp/hbase-stackable/local/jars/hbase-gis-1.0.0.jar:/stackable/conf' \
+    tech.stackable.gis.hbase.WithinQuery \
+    remote \
+    "POLYGON ((-76.0 44.97, \
+        -76.0 45.0, \
+        -75.9 45.0, \
+        -75.9 44.97, \
+        -76.0 44.97))"
+
+You should a subset of the bulk-loaded back as the results of this query (due to the way that the query polygon and the limits of the randomly generated data have been defined.)
 
 ## Using the KNNFilter
 
--- src/main/java/tech/stackable/gis/hbase/BulkIngest.java
+++ src/main/java/tech/stackable/gis/hbase/BulkIngest.java
@@ -8,79 +8,88 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.PrimitiveIterator;
+import java.util.Random;
 
 public class BulkIngest {
 
-  private static final String usage = "ingest table source.tsv\n" +
-      "  help - print this message and exit.\n" +
-      "  table - the target table to load.\n" +
-      "  family - the column family to use.\n" +
-      "  count - the number of rows to generate and load.\n" +
-      "\n" +
-      "generates a geohash for the rowkey.\n" +
-      "records are stored in columns in the 'a' family, columns are:\n" +
-      "  lon,lat,id,name,address,city,url,phone,type,zip\n";
+    private static final String usage = "ingest table source.tsv\n" +
+            "  help - print this message and exit.\n" +
+            "  table - the target table to load.\n" +
+            "  family - the column family to use.\n" +
+            "  count - the number of rows to generate and load.\n" +
+            "  limits - the applied co-ordinate ranage in the form: lon_min,lon_max,lat_min,lat_max\n" +
+            "\n" +
+            "generates a geohash for the rowkey.\n" +
+            "records are stored in columns in the 'a' family, columns are:\n" +
+            "  lon,lat,id,name,address,city,url,phone,type,zip\n";
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-    if (args.length != 3) {
-      System.out.println(usage);
-      System.exit(0);
+        if (args.length != 4) {
+            System.out.println(usage);
+            System.exit(0);
+        }
+
+        final TableName tableName = TableName.valueOf(args[0]);
+        final Configuration conf = HBaseConfiguration.create();
+
+        HBaseAdmin.available(conf);
+        Connection connection = ConnectionFactory.createConnection(conf);
+        Table table = connection.getTable(tableName);
+
+        final Admin admin = connection.getAdmin();
+        byte[] family = String.valueOf(args[1]).getBytes();
+        int row_count = Integer.parseInt(args[2]);
+        long start = System.currentTimeMillis();
+        String[] limits = String.valueOf(args[3]).split(",");
+
+        // we need all 4
+        if (limits.length != 4) {
+            System.out.println(usage);
+            System.exit(0);
+        }
+
+        // generate random co-ordinates within a fixed region
+        PrimitiveIterator.OfDouble lon_iter = new Random().doubles(Double.valueOf(limits[0]), Double.valueOf(limits[1])).iterator();
+        PrimitiveIterator.OfDouble lat_iter = new Random().doubles(Double.valueOf(limits[2]), Double.valueOf(limits[3])).iterator();
+
+        try {
+            for (int i = 0; i < row_count; i++) {
+                Put put = getPut(family, i, lon_iter, lat_iter);
+                table.put(put);
+            }
+
+            admin.flush(tableName);
+        } finally {
+            table.close();
+            connection.close();
+            admin.close();
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.printf("Geohashed %s records in %sms.%n", row_count, end - start);
     }
 
-    final TableName tableName = TableName.valueOf(args[0]);
-    final Configuration conf = HBaseConfiguration.create();
+    public static Put getPut(byte[] family, int id, PrimitiveIterator.OfDouble lon_iter, PrimitiveIterator.OfDouble lat_iter) {
+        double lon = lon_iter.nextDouble();
+        double lat = lat_iter.nextDouble();
+        int randomTextLength = 10;
 
-    HBaseAdmin.available(conf);
-    Connection connection = ConnectionFactory.createConnection(conf);
-    Table table = connection.getTable(tableName);
+        String rowkey = GeoHash.withCharacterPrecision(lat, lon, 12).toBase32();
+        Put put = new Put(rowkey.getBytes());
+        put.setDurability(Durability.SKIP_WAL);
 
-    final Admin admin = connection.getAdmin();
-    byte[] family = String.valueOf(args[1]).getBytes();
-    int row_count = Integer.parseInt(args[2]);
-    long start = System.currentTimeMillis();
-
-    // generate random co-ordinates within a fixed region
-    PrimitiveIterator.OfDouble lon_iter = new Random().doubles(-75.99, -75.01).iterator();
-    PrimitiveIterator.OfDouble lat_iter = new Random().doubles(44.01, 44.99).iterator();
-
-    try {
-      for (int i = 0; i < row_count; i++) {
-        Put put = getPut(family, i, lon_iter, lat_iter);
-        table.put(put);
-      }
-
-      admin.flush(tableName);
-    } finally {
-      table.close();
-      connection.close();
-      admin.close();
+        put.addColumn(family, "id".getBytes(), Integer.toString(id).getBytes());
+        put.addColumn(family, "lon".getBytes(), Double.toString(lon).getBytes());
+        put.addColumn(family, "lat".getBytes(), Double.toString(lat).getBytes());
+        put.addColumn(family, "name".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        put.addColumn(family, "address".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        put.addColumn(family, "city".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        put.addColumn(family, "url".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        put.addColumn(family, "phone".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        put.addColumn(family, "type".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        put.addColumn(family, "zip".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
+        return put;
     }
-
-    long end = System.currentTimeMillis();
-    System.out.printf("Geohashed %s records in %sms.%n", row_count, end - start);
-  }
-
-  public static Put getPut(byte[] family, int id, PrimitiveIterator.OfDouble lon_iter, PrimitiveIterator.OfDouble lat_iter) {
-    double lon = lon_iter.nextDouble();
-    double lat = lat_iter.nextDouble();
-    int randomTextLength= 10;
-
-    String rowkey = GeoHash.withCharacterPrecision(lat, lon, 12).toBase32();
-    Put put = new Put(rowkey.getBytes());
-    put.setDurability(Durability.SKIP_WAL);
-
-    put.addColumn(family, "id".getBytes(), Integer.toString(id).getBytes());
-    put.addColumn(family, "lon".getBytes(), Double.toString(lon).getBytes());
-    put.addColumn(family, "lat".getBytes(), Double.toString(lat).getBytes());
-    put.addColumn(family, "name".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    put.addColumn(family, "address".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    put.addColumn(family, "city".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    put.addColumn(family, "url".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    put.addColumn(family, "phone".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    put.addColumn(family, "type".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    put.addColumn(family, "zip".getBytes(), RandomStringUtils.randomAlphabetic(randomTextLength).getBytes());
-    return put;
-  }
 }
